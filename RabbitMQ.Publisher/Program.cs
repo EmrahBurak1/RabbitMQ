241a26674b20974c8ed2175cc7e9e9c6c085c656
Emrah Burak Kaya <burak.kaya@brisa.com.tr>
2024-09-30 13:40:22 +0300

    RabbitMQ Projesi

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -2,35 +2,62 @@
 using RabbitMQ.Client;
 using System.Text;
 
-var factory = new ConnectionFactory{ HostName = "localhost" };
-//factory.Uri = new Uri("http://localhost:5672"); //Rabbitmq'yu docker üzerinden ayağa kaldırdık. Dockerda container'ın ip adresi 172.17.0.2'dir. 
+#region Exchange olmadan rabbitmq kullanımı. (Default exchange olarak geçiyor)
+//var factory = new ConnectionFactory{ HostName = "localhost" };
+////factory.Uri = new Uri("http://localhost:5672"); //Rabbitmq'yu docker üzerinden ayağa kaldırdık. Dockerda container'ın ip adresi 172.17.0.2'dir. 
+
+//var connection = factory.CreateConnection(); //Bağlantıyı sağladık.
+
+//var channel = connection.CreateModel(); //Yeni bir kanal oluşturduk.
+
+////İlk parametre queue yani kuyruğun ismi
+////ikinci parametre durable yani true false'a göre memoryde tutulup tutulmamasını belirler. True yaparsak fiziksel olarak kaydedilir rabbitmq restartlansa bile kuyruk kaybolmaz.
+////üçüncü parametre exclusive yani true yaparsak burdaki kuyruğa bu projede oluşturduğumuz kanal ile bağlantı kurabilirim anlamına geliyor. Biz buradaki kuyruğa subscriber üzerinden farklı bir kanaldan bağlanacağımız için false yapıyoruz.
+////dördüncü parametre autodelete bu parametrenin yaptığı işlem ise bu kuyruğa bağlı olan son subscriber da kuyruğa bağlantısını kapatırsa bu kuyruk otomatik olarak silinir.
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+////Foreach ile bir kerede 50 tane mesaj göndericez.
+//Enumerable.Range(1, 50).ToList().ForEach(x =>
+//{
+//    string message = $"Message {x}";
+
+//    var messageBody = Encoding.UTF8.GetBytes(message); //Rabbitmq da mesajlar byte olarak gönderilir.
+
+//    //İlk parametre exchange kullanmadığımız için default exchange olarak geçer.
+//    //İkinci parametre routing key olarak kuyruk ismi verilir. 
+//    //Üçüncü parametre olarak basic propertyleri null geçtik.
+//    //Dördüncü parametre ôlarak byte dizisi olarak body'yi veririz.
+//    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+//    Console.WriteLine($"Mesaj gönderilmiştir.: {message}");
+//});
+
+//Console.ReadLine();
+#endregion
+
+#region Fanout Exchange ile rabbitmq kullanımı.
+var factory = new ConnectionFactory { HostName = "localhost" };
 
 var connection = factory.CreateConnection(); //Bağlantıyı sağladık.
 
 var channel = connection.CreateModel(); //Yeni bir kanal oluşturduk.
 
-//İlk parametre queue yani kuyruğun ismi
-//ikinci parametre durable yani true false'a göre memoryde tutulup tutulmamasını belirler. True yaparsak fiziksel olarak kaydedilir rabbitmq restartlansa bile kuyruk kaybolmaz.
-//üçüncü parametre exclusive yani true yaparsak burdaki kuyruğa bu projede oluşturduğumuz kanal ile bağlantı kurabilirim anlamına geliyor. Biz buradaki kuyruğa subscriber üzerinden farklı bir kanaldan bağlanacağımız için false yapıyoruz.
-//dördüncü parametre autodelete bu parametrenin yaptığı işlem ise bu kuyruğa bağlı olan son subscriber da kuyruğa bağlantısını kapatırsa bu kuyruk otomatik olarak silinir.
-channel.QueueDeclare("hello-queue", true, false, false);
+//channel.QueueDeclare("hello-queue", true, false, false); //Kuyruğu artık publisher olarak oluşturmucaz. Subscriber oluşturacak.
 
-//Foreach ile bir kerede 50 tane mesaj göndericez.
+//İlk parametrede bir isim veriyoruz, ikinci parametre durable fiziksel olarak kaydedilsin. Uygulamaya restart atsakta exchange kaybolmasın. 3.parametre ile de exchange tipi belirtiliyor.
+channel.ExchangeDeclare("log-fanout", durable: true, type: ExchangeType.Fanout);
+
 Enumerable.Range(1, 50).ToList().ForEach(x =>
 {
-    string message = $"Message {x}";
+    string message = $"log {x}";
 
     var messageBody = Encoding.UTF8.GetBytes(message); //Rabbitmq da mesajlar byte olarak gönderilir.
 
-    //İlk parametre exchange kullanmadığımız için default exchange olarak geçer.
-    //İkinci parametre routing key olarak kuyruk ismi verilir. 
-    //Üçüncü parametre olarak basic propertyleri null geçtik.
-    //Dördüncü parametre ôlarak byte dizisi olarak body'yi veririz.
-    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    //BasicPublish aşamasında artık ilk parametre olarak yukarıda declare ettiğimiz exchange ismini burada veriyoruz. İkinci parametre olarak kuyruk ismini vermiştik artık boşş olarak bırakıyoruz.
+    channel.BasicPublish("log-fanout", "", null, messageBody);
 
     Console.WriteLine($"Mesaj gönderilmiştir.: {message}");
 });
 
-
-
 Console.ReadLine();
+#endregion
--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -3,33 +3,73 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
+#region Exchange olmadan rabbitmq kullanımı. (Default exchange olarak geçiyor)
+//var factory = new ConnectionFactory { HostName = "localhost" };
+
+//var connection = factory.CreateConnection(); //Bağlantıyı sağladık.
+
+//var channel = connection.CreateModel(); //Yeni bir kanal oluşturduk.
+
+////İlk parametre queue yani kuyruğun ismi
+////ikinci parametre durable yani true false'a göre memoryde tutulup tutulmamasını belirler. True yaparsak fiziksel olarak kaydedilir rabbitmq restartlansa bile kuyruk kaybolmaz.
+////üçüncü parametre exclusive yani true yaparsak burdaki kuyruğa bu projede oluşturduğumuz kanal ile bağlantı kurabilirim anlamına geliyor. Biz buradaki kuyruğa subscriber üzerinden farklı bir kanaldan bağlanacağımız için false yapıyoruz.
+////dördüncü parametre autodelete bu parametrenin yaptığı işlem ise bu kuyruğa bağlı olan son subscriber da kuyruğa bağlantısını kapatırsa bu kuyruk otomatik olarak silinir.
+////channel.QueueDeclare("hello-queue", true, false, false); //Hem publisher hem subscriber tarafında kuyruk tanımlıyorsak ikisinin de parametreleri aynı olmalı.
+
+////Burada da her bir subscriber'a kaç mesaj gelsin onu belirleyebiliyoruz. Örneğin 50 mesajımız var bunu 5 subscriber'a 10'ar 10'ar göndermesini sağlayabiliyoruz.
+////İlk parametreye sıfır yazıyoruz yani herhangi bir boyuttaki mesajı gönderebiliriz anlamına geliyor.
+////İkinci parametre ise her subscriber'a 1'er tane mesaj gelsin anlamına geliyor.
+////Üçüncü parametre olan global false olursa her bir subscriber'a kaç adet gönderileceğini belirtir. Yani 2.parametreyi 5 yapıp 3.paramatre false olursa her bir subscriber'a 5'er 5'er gönder anlamına gelir true yaparsakta örneğin 2.parametre 6 ve 2 tane de subscriber var bunu 3'er 3'er bölürek her bir sub'a gönderir. Mesela true deyip 2. parametreye 3 yazarsak iki tane de sub'ımız varsa 2 yukardakine 1 aşağıdakine gönderir toplamı 3 olur.
+//channel.BasicQos(0, 1, false);
+
+//var consumer = new EventingBasicConsumer(channel);
+
+////İlk parametremiz kuyruğun ismi olur.
+////ikinci parametremiz olan autoAck parametresi true olursa bu mesaj doğruda işlense yanlışta işlence kuyruktan direkt siler. False yapılırsa kuyruktan silmez doğru bir şekilde işlerse biz silmesi için komut vericez anlamına gelir.
+////üçüncü parametre ise consumer'dır.
+//channel.BasicConsume("hello-queue", false, consumer);
+
+////Bu event rabbitmq bu subscriber'a bir mesaj gönderdiğinde bu event fırlatılıyor.
+////Yani gelen mesajları bu şekilde dinliyoruz.
+//consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+//{
+//    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+//    Thread.Sleep(1000); //Görevilmek için 1 saniyelik gecikme verdik.
+
+//    Console.WriteLine("Gelen Mesaj:" + message);
+
+//    //BasicConsume içinde autoAct ifadesini false yapınca otomatik olarak silmeyecek. Biz burada gelen mesajı işledikten sonra silmesini istiyoruz. O yüzden basci act ile yapabiliriz.
+//    //İlk parametre DeliveryTag'ın anlamı ilgili mesajı artık silebiliriz anlamına geliyor. Yani bize ulaştırılan tag'ı rabbitmq'ya gönderiyoruz sonra hangi tag ile o mesaj bize ulaştırılmışsa ilgili mesajı bulup kuyruktan siliyor.
+//    //İkinci parametre multiple, true dersek o anda memory de işlenmiş ama rabbitmq'ya gitmemiş başka mesajlar da varsa rabbitmq ya haber verir. False ise sadece ilgili mesajın durumunu rabbitmq ya bildiriyoruz.
+//    channel.BasicAck(e.DeliveryTag,false);
+//};
+
+//Console.ReadLine();
+#endregion
+
+//4 tane exchange tipi vardır.
+//1. Fanout Exchange: Herhangi bir filtre yapmadan kendisine bağlı olan tüm kuyruklara mesajları iletir.
+//2. Direct Exchange:
+//3. Topic Exchange:
+//4. Header Exchange:
+
+#region Fanout Exchange ile rabbitmq kullanımı. 
 var factory = new ConnectionFactory { HostName = "localhost" };
 
 var connection = factory.CreateConnection(); //Bağlantıyı sağladık.
 
 var channel = connection.CreateModel(); //Yeni bir kanal oluşturduk.
 
-//İlk parametre queue yani kuyruğun ismi
-//ikinci parametre durable yani true false'a göre memoryde tutulup tutulmamasını belirler. True yaparsak fiziksel olarak kaydedilir rabbitmq restartlansa bile kuyruk kaybolmaz.
-//üçüncü parametre exclusive yani true yaparsak burdaki kuyruğa bu projede oluşturduğumuz kanal ile bağlantı kurabilirim anlamına geliyor. Biz buradaki kuyruğa subscriber üzerinden farklı bir kanaldan bağlanacağımız için false yapıyoruz.
-//dördüncü parametre autodelete bu parametrenin yaptığı işlem ise bu kuyruğa bağlı olan son subscriber da kuyruğa bağlantısını kapatırsa bu kuyruk otomatik olarak silinir.
-//channel.QueueDeclare("hello-queue", true, false, false); //Hem publisher hem subscriber tarafında kuyruk tanımlıyorsak ikisinin de parametreleri aynı olmalı.
+//İlk parametrede bir isim veriyoruz, ikinci parametre durable fiziksel olarak kaydedilsin. Uygulamaya restart atsakta exchange kaybolmasın. 3.parametre ile de exchange tipi belirtiliyor.
+channel.ExchangeDeclare("log-fanout", durable: true, type: ExchangeType.Fanout);
 
-//Burada da her bir subscriber'a kaç mesaj gelsin onu belirleyebiliyoruz. Örneğin 50 mesajımız var bunu 5 subscriber'a 10'ar 10'ar göndermesini sağlayabiliyoruz.
-//İlk parametreye sıfır yazıyoruz yani herhangi bir boyuttaki mesajı gönderebiliriz anlamına geliyor.
-//İkinci parametre ise her subscriber'a 1'er tane mesaj gelsin anlamına geliyor.
-//Üçüncü parametre olan global false olursa her bir subscriber'a kaç adet gönderileceğini belirtir. Yani 2.parametreyi 5 yapıp 3.paramatre false olursa her bir subscriber'a 5'er 5'er gönder anlamına gelir true yaparsakta örneğin 2.parametre 6 ve 2 tane de subscriber var bunu 3'er 3'er bölürek her bir sub'a gönderir. Mesela true deyip 2. parametreye 3 yazarsak iki tane de sub'ımız varsa 2 yukardakine 1 aşağıdakine gönderir toplamı 3 olur.
 channel.BasicQos(0, 1, false);
 
 var consumer = new EventingBasicConsumer(channel);
 
-//İlk parametremiz kuyruğun ismi olur.
-//ikinci parametremiz olan autoAck parametresi true olursa bu mesaj doğruda işlense yanlışta işlence kuyruktan direkt siler. False yapılırsa kuyruktan silmez doğru bir şekilde işlerse biz silmesi için komut vericez anlamına gelir.
-//üçüncü parametre ise consumer'dır.
 channel.BasicConsume("hello-queue", false, consumer);
 
-//Bu event rabbitmq bu subscriber'a bir mesaj gönderdiğinde bu event fırlatılıyor.
-//Yani gelen mesajları bu şekilde dinliyoruz.
 consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
 {
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
@@ -38,10 +78,8 @@ consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
 
     Console.WriteLine("Gelen Mesaj:" + message);
 
-    //BasicConsume içinde autoAct ifadesini false yapınca otomatik olarak silmeyecek. Biz burada gelen mesajı işledikten sonra silmesini istiyoruz. O yüzden basci act ile yapabiliriz.
-    //İlk parametre DeliveryTag'ın anlamı ilgili mesajı artık silebiliriz anlamına geliyor. Yani bize ulaştırılan tag'ı rabbitmq'ya gönderiyoruz sonra hangi tag ile o mesaj bize ulaştırılmışsa ilgili mesajı bulup kuyruktan siliyor.
-    //İkinci parametre multiple, true dersek o anda memory de işlenmiş ama rabbitmq'ya gitmemiş başka mesajlar da varsa rabbitmq ya haber verir. False ise sadece ilgili mesajın durumunu rabbitmq ya bildiriyoruz.
-    channel.BasicAck(e.DeliveryTag,false);
+    channel.BasicAck(e.DeliveryTag, false);
 };
 
 Console.ReadLine();
+#endregion
